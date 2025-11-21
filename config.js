0fb321ad6e05b819f73e09aba36411328c7cc868
iMayhem <67825237+iMayhem@users.noreply.github.com>
2025-11-21 19:28:28 +0530

    testing

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://czeebkyxfsgwxxsailkn.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZWVia3l4ZnNnd3h4c2FpbGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MjcxODAsImV4cCI6MjA3OTMwMzE4MH0.cFcEXDFmFiBAJErL8UIFpsNtGkjtpstwYdJSfI5Pepc';
+
+// Helper to init
+function getDB() {
+    return supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+}
