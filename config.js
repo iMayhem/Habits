a5bb9b9ff7a7e33a3e0e0f705cbf1364908c811f
iMayhem <67825237+iMayhem@users.noreply.github.com>
2025-11-21 21:07:20 +0530

    testin 7

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Replace with your actual keys
+// Paste your Keys here (Make sure URL is https://something.supabase.co)
 const SUPABASE_URL = 'https://czeebkyxfsgwxxsailkn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZWVia3l4ZnNnd3h4c2FpbGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MjcxODAsImV4cCI6MjA3OTMwMzE4MH0.cFcEXDFmFiBAJErL8UIFpsNtGkjtpstwYdJSfI5Pepc';
 
