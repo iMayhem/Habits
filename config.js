3d1ebcf9dce9e7a96d994f3224f3e50624013b5f
iMayhem <67825237+iMayhem@users.noreply.github.com>
2025-11-21 22:11:32 +0530

    testing

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Replace with your keys
 const SUPABASE_URL = 'https://czeebkyxfsgwxxsailkn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZWVia3l4ZnNnd3h4c2FpbGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MjcxODAsImV4cCI6MjA3OTMwMzE4MH0.cFcEXDFmFiBAJErL8UIFpsNtGkjtpstwYdJSfI5Pepc';
 
